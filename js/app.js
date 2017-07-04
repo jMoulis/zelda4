d7760779c05a9ef38615026601133f3f239c7eff
jMoulis <julien.moulis@moulis.me>
2017-07-04 12:12:41 +0200

    Générateur Map

--- js/app.js
+++ js/app.js
@@ -1,23 +1,33 @@
-var app = {
+const app = {
   /*
    * 1. Dans js/app.js, on va créer un objet app responsable de l'ajout des objets dans le DOM. On va créer un conteneur #map.
    * 2. L'élément #map doit aussi recevoir en style des dimensions, en fonction de la longueur de l'objet map (de js/map.js). Chaque tuile est un carré 16 pixels.
    */
   init: function() {
-
+		console.log('Zelda 4... The game starts');
+		app.$gamewrapper = $('#game-wrapper');
+		app.createMap();
+		app.createTiles();
   },
 
-
+	createMap: function(){
+		app.$map = $('<div>');
+		app.$map.attr('id', 'map');
+		app.$gamewrapper.append(app.$map);
+	},
   /*
    * 3. Dans #map, on va venir ajouter en enfant toutes les tuiles .tile.
    */
   createTiles: function() {
+		for (let line = 0; line < map.tiles.length; line++) {
+      for (let column = 0; column < map.tiles[line].length; column++) {
+        tile.create(column, line);
+      }
+    }
+	}
 
-  },
 }
-
-
 /*
  * Chargement du DOM
  */
-document.addEventListener('DOMContentLoaded', app.init);
+$(app.init);
--- js/tile.js
+++ js/tile.js
@@ -2,31 +2,71 @@ var tile = {
   /*
    * 1. Dans js/tile.js, on va créer un objet tile responsable de la création des tuiles. On peut créer une méthode create chargé de créer une tuile.
    */
-  create: function(posX, posY) {
-
+  create: function(line, column) {
+		tile.$tile = $('<div>');
+		tile.$tile
+		.addClass(tile.getClasses(line, column))
+		.css(tile.getStyle(line, column))
+		.attr('data-line', line).attr('data-column', column);
+		app.$map.append(tile.$tile);
   },
 
 
   /*
    * 2. Pour créer une tuile, il faut lui donner les bonnes classes, comme décrit plus haut. Pour cela, on peut créer une méthode getClassname.
    */
-  getClasses: function(posX, posY) {
-
+  getClasses: function(column, line) {
+    let classes = 'tile '+ tile.getType(column, line).name +' '+tile.getType(column, line).perm ;
+		 // on regarde son type en fonction de X et Y
+    classes += ' ';
+    // top
+		if(line > 0){
+			classes += ' top-'+ tile.getType(column, line-1).name;
+		}
+		if(column + 1 < map.tiles[line].length){
+			classes += ' right-'+ tile.getType(column+1, line).name;
+		} else {
+			classes += ''
+		}
+		if(line + 1 < map.tiles.length) {
+			classes += ' bottom-'+ tile.getType(column, line+1).name;
+		} else {
+			classes += ''
+		}
+		if(column > 0){
+			classes += ' left-'+ tile.getType(column-1, line).name;
+		} else {
+			classes += ' no-left'
+		}
+    return classes;
   },
 
 
   /*
    * 3. Pour venir positionner les tuiles, on utilise des position: absolute. Il faut donc ajouter du style (top et left) pour venir placer la tuile au bon endroit, avec une méthode getStyle.
    */
-  getStyle: function(posX, posY) {
-
+  getStyle: function(column, line) {
+		return style = {
+			left: column*16,
+			top: line*16
+		}
   },
-
 
   /*
    * 4. Pour chaque tuile, on peut aussi utiliser une méthode getType qui sera chargé de récupérer le type de tuile, en utilisant l'objet map.
    */
-  getType: function(posX, posY) {
+  getType: function(column, line) {
+		let char =	map.tiles[line][column];
+		let arrayTile = [];
+		//console.log(char);
+			//console.log(typeTile = map.types[char]);
+			$.each(map.types, function(key, value){
+				arrayTile[value.sign] = { name: value.name, perm: value.permission};
+			});
+
+		let typeTile = arrayTile[char];
+
+		return typeTile;
 
   },
 };
